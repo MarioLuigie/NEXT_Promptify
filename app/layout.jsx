5952c36c4d925c16e61356c81ed2d45464a44e1b
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-05-26 23:42:28 +0200

    Init files

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+import '@/styles/globals.css'
+
+export const metadata = {
+  title: 'Promptify',
+  description: 'AI application for using prompts'
+}
+
+export default function RootLayout({ children }) {
+	return <div>{children}</div>
+}
