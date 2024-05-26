3e6c4d95c2cd396d94b0268878198657963066f2
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-05-26 23:50:17 +0200

    Feat: Create root page and layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,19 @@
 import '@/styles/globals.css'
 
 export const metadata = {
-  title: 'Promptify',
-  description: 'AI application for using prompts'
+	title: 'Promptify',
+	description: 'Discover and Share AI Prompts',
 }
 
 export default function RootLayout({ children }) {
-	return <div>{children}</div>
+	return (
+		<html lang="en">
+			<body>
+				<div className="main">
+					<div className="gradient" />
+				</div>
+				<main className="app">{children}</main>
+			</body>
+		</html>
+	)
 }
