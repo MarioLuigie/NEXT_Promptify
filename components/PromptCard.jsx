a0497f0dbf43d8bd5e25b1704242fd56eeacdb4f
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-06-02 21:21:52 +0200

    Styles: Style PromptCard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,10 +6,16 @@ import { useSession } from 'next-auth/react'
 import { usePathname, useRouter } from 'next/navigation'
 
 export default function PromptCard({ post, handleTagClick }) {
-  const handleProfileClick = () => {
+	const [copied, setCopied] = useState('')
 
-  }
-  
+	const handleProfileClick = () => {}
+
+	const handleCopy = () => {
+		setCopied(post.prompt)
+		navigator.clipboard.writeText(post.prompt)
+		setTimeout(() => setCopied(''), 3000)
+	}
+
 	return (
 		<div className="prompt_card">
 			<div className="flex justify-between items-start gap-5">
@@ -25,10 +31,38 @@ export default function PromptCard({ post, handleTagClick }) {
 						className="rounded-full object-contain"
 					/>
 				</div>
+
+				<div className="flex flex-col">
+					<h3 className="font-satoshi font-semibold text-gray-900">
+						{post.creator.username}
+					</h3>
+					<p className="font-inter text-sm text-gray-500">
+						{post.creator.email}
+					</p>
+				</div>
+
+				<div className="copy_btn" onClick={handleCopy}>
+					<Image
+						src={
+							copied === post.prompt
+								? '/assets/icons/tick.svg'
+								: '/assets/icons/copy.svg'
+						}
+						alt={copied === post.prompt ? 'tick_icon' : 'copy_icon'}
+						width={20}
+						height={20}
+					/>
+				</div>
 			</div>
-			<p>{post.prompt}</p>
-			<p>{post.tag}</p>
-			<p>{post.creator.username}</p>
+			<p className="my-4 font-satoshi text-sm text-gray-700">
+				{post.prompt}
+			</p>
+			<p
+				className="font-inter text-sm text-orange-400 cursor-pointer"
+				onClick={() => handleTagClick && handleTagClick(post.tag)}
+			>
+				#{post.tag}
+			</p>
 		</div>
 	)
 }
