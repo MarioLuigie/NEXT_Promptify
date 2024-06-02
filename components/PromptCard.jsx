034d22eea2adecc17a15cf204a1c3776a1930fba
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-06-02 18:47:57 +0200

    Feat: Add profile image to PromptCard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,6 +1,31 @@
+'use client'
+
+import { useState } from 'react'
+import Image from 'next/image'
+import { useSession } from 'next-auth/react'
+import { usePathname, useRouter } from 'next/navigation'
+
 export default function PromptCard({ post, handleTagClick }) {
+  const handleProfileClick = () => {
+
+  }
+  
 	return (
-		<div>
+		<div className="prompt_card">
+			<div className="flex justify-between items-start gap-5">
+				<div
+					className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+					onClick={handleProfileClick}
+				>
+					<Image
+						src={post.creator.image}
+						alt="Creator image"
+						width={37}
+						height={37}
+						className="rounded-full object-contain"
+					/>
+				</div>
+			</div>
 			<p>{post.prompt}</p>
 			<p>{post.tag}</p>
 			<p>{post.creator.username}</p>
