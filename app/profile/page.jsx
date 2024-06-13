90ec0012766c7c8177b4aa79e5857d7e52f26dca
Mariusz Łotocki <mk.lotocki@gmail.com>
2024-06-13 12:40:49 +0200

    Refactor: Move logical from ProfilePage to Profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,61 +1,8 @@
-'use client'
-
-import { Suspense, useEffect, useState } from 'react'
-import { useSession } from 'next-auth/react'
 import Profile from '@/components/Profile'
-import { useSearchParams } from 'next/navigation'
-
-function MyProfilePage() {
-	const [posts, setPosts] = useState([])
-	const [postCreator, setPostCreator] = useState({})
-	const { data: session } = useSession()
-
-	const searchParams = useSearchParams()
-	const postCreatorId = searchParams.get('user')
-
-	// console.log('LOG Z PROFILE PAGE:', userId)
-
-	const fetchPosts = async () => {
-		const res = await fetch(`/api/users/${postCreatorId}/posts`)
-		const data = await res.json()
-		setPosts(data)
-	}
-
-	const fetchUser = async () => {
-		const res = await fetch(`/api/users/${postCreatorId}`)
-		const data = await res.json()
-		setPostCreator(data)
-	}
-
-	useEffect(() => {
-		if (session?.user.id) {
-			fetchPosts()
-			fetchUser()
-		}
-	}, [session?.user.id])
-
-	console.log('LOG Z PROFILE PAGE:', postCreator)
-
-	return (
-		<Profile
-			name={
-        session?.user.id === postCreatorId 
-        ? 'My' 
-        : postCreator?.username 
-            ? postCreator.username.charAt(0).toUpperCase() + postCreator.username.slice(1)
-            : ''
-    }
-			desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
-			posts={posts}
-			setPosts={setPosts}
-		/>
-	)
-}
 
 export default function ProfilePage() {
 	return (
-		<Suspense fallback={<div>Loading page...</div>}>
-			<MyProfilePage />
-		</Suspense>
+		<Profile />
 	)
 }
+
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,26 +1,99 @@
-import PromptCard from '@/components/PromptCard'
+'use client'
 
-export default function Profile({
-	name,
-	desc,
-	posts,
-	setPosts
-}) {
+import PromptCard from '@/components/PromptCard'
+import { Suspense, useEffect, useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { useSearchParams } from 'next/navigation'
+
+export default function Profile() {
+	const [posts, setPosts] = useState([])
+	const [user, setUser] = useState({})
+
+	const { data: session, status } = useSession()
+
+	const searchParams = useSearchParams()
+	const userId = searchParams.get('user')
+
+	const fetchPosts = async () => {
+		const res = await fetch(`/api/users/${userId}/posts`)
+		const data = await res.json()
+		setPosts(data)
+	}
+
+	const fetchUser = async () => {
+		const res = await fetch(`/api/users/${userId}`)
+		const data = await res.json()
+		setUser(data)
+	}
+
+	useEffect(() => {
+		if (userId) {
+			fetchPosts()
+			fetchUser()
+		}
+	}, [userId])
+
+	const capitalizeName = (name) => {
+		if (!name) return ''
+		return name.charAt(0).toUpperCase() + name.slice(1)
+	}
+
+	if (status === 'loading') {
+		return <div>Loading...</div>
+	}
+
 	return (
-		<section className="w-full">
-			<h1 className="head_text text-left">{name} Profile</h1>
-			<p className="desc text-left">{desc}</p>
-			<div className="mt-16 prompt_layout">
-				{posts.length > 0 &&
-					posts.map((post) => (
-						<PromptCard
-							key={post._id}
-							post={post}
-							setPosts={setPosts}
-							posts={posts}
-						/>
-					))}
-			</div>
-		</section>
+		<Suspense fallback={<div>Loading page...</div>}>
+			<section className="w-full">
+				<h1 className="head_text text-left">
+					{session && session.user && session.user.id === userId
+						? 'My'
+						: `${capitalizeName(user.username)}'s`}{' '}
+					Profile
+				</h1>
+				<p className="desc text-left">
+					Welcome to your personalized profile page. Share your exceptional
+					prompts and inspire others with the power of your imagination
+				</p>
+				<div className="mt-16 prompt_layout">
+					{posts.length > 0 &&
+						posts.map((post) => (
+							<PromptCard
+								key={post._id}
+								post={post}
+								setPosts={setPosts}
+								posts={posts}
+							/>
+						))}
+				</div>
+			</section>
+		</Suspense>
 	)
 }
+
+// import PromptCard from '@/components/PromptCard'
+
+// export default function Profile({
+// 	name,
+// 	desc,
+// 	posts,
+// 	setPosts
+// }) {
+// 	return (
+// 		<section className="w-full">
+// 			<h1 className="head_text text-left">{name} Profile</h1>
+// 			<p className="desc text-left">{desc}</p>
+// 			<div className="mt-16 prompt_layout">
+// 				{posts.length > 0 &&
+// 					posts.map((post) => (
+// 						<PromptCard
+// 							key={post._id}
+// 							post={post}
+// 							setPosts={setPosts}
+// 							posts={posts}
+// 						/>
+// 					))}
+// 			</div>
+// 		</section>
+// 	)
+// }
